03a3e418cb9abe3d172879762a5da11bc5555688
Ephrem Binchebo <ephremgensa1@gmail.com>
2022-08-18 16:51:46 -0400

    init

---
+++ HomeProject1/src/test/java/com/company/HomeProject1/HomeProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.HomeProject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
